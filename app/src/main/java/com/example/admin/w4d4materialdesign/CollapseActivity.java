10cb16d5337f10b271ba55c5eb0eb408b1007a1a
eguegan <e.guegan@margulls.com>
2016-04-21 15:16:08 -0400

    add collapse bar

--- app/src/main/java/com/example/admin/w4d4materialdesign/CollapseActivity.java
+++ app/src/main/java/com/example/admin/w4d4materialdesign/CollapseActivity.java
@@ -21,25 +21,7 @@ public class CollapseActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_collapse);
 
-        mToolbar = (Toolbar) findViewById(R.id.a_main_toolbar);
-        setSupportActionBar(mToolbar);
 
-        mArrayList = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            mArrayList.add("Hello");
-            mArrayList.add("World");
-            mArrayList.add("Why");
-            mArrayList.add("Always");
-            mArrayList.add("Me");
-        }
-
-        mBasicAdapter = new BasicAdapter(mArrayList);
-
-        mRecyclerView = (RecyclerView) findViewById(R.id.a_main_recycler);
-
-        mRecyclerView.setAdapter(mBasicAdapter);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
-        mRecyclerView.addItemDecoration(new SimpleDecorator(10));
 
     }
 }
